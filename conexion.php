ef14d003df4057b48629707eaa7f2463ab0b2c3d
anibal234 <gmoralesa6@miumg.edu.gt>
2025-02-23 12:14:55 -0600

    prueba

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";       
+$password = "";           
+$dbname = "clinicadiabetes"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+} else {
+    //echo "Conexión exitosa<br>";
+}
+?>
