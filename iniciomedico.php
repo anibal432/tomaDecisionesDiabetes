3fc25a1f9415b7ed0c562b9598d4474bd8bc49a8
miguelgarcia7810@gmail.com <miguelgarcia7810@gmail.com>
2025-03-14 17:58:05 -0600

    menu

--- iniciomedico.php
+++ iniciomedico.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicio - Medicos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
-    <p>si usted esta viendo esto es por que es Medico</p>
+    <nav class="navbar">
+        <div class="logo">Diabetes Log</div>
+        <ul>
+            <li><a href="iniciomedico.php"><i class="fas fa-home"></i>Inicio</a></li>
+            <li><a href="#"><i class="fas fa-solid fa-users"></i>Boton 1</a></li>
+            <li><a href="#"><i class="fas fa-solid fa-hospital-user"></i>Boton 2</a></li>
+        </ul>
+    </nav>
+    <br>
+    <p>si usted esta viendo esto es medico</p>
 </body>
 </html>
--- iniciosecre.php
+++ iniciosecre.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicio - Secretari@s</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
-    <p>si usted esta viendo esto es por que es secretari@</p>
+    <nav class="navbar">
+        <div class="logo">Admin Log</div>
+        <ul>
+            <li><a href="iniciosecre.php"><i class="fas fa-home"></i>Inicio</a></li>
+            <li><a href="#"><i class="fas fa-solid fa-users"></i>Boton 1</a></li>
+            <li><a href="#"><i class="fas fa-solid fa-hospital-user"></i>Boton 2</a></li>
+        </ul>
+    </nav>
+    <br>
+    <p>si usted esta viendo esto es secretari@</p>
 </body>
 </html>
