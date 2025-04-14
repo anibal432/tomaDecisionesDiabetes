393ea8bc17753daa3326124b115940dd97b777c5
Incubuz42 <josue12.gonzalez42@gmail.com>
2025-04-13 18:27:24 -0600

    Update conexion.php
    ARG

--- Medico/conexion.php
+++ Medico/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "167.86.71.203"; // Cambia esto si tu base de datos está en otro servidor
-$user = "allen"; // Usuario de la base de datos
-$password = "patopatopato1"; // Contraseña de la base de datos (déjala vacía si no tiene)
-$database = "DiabetesUmg"; // Nombre de la base de datos
+$host = "167.86.71.203";
+$user = "allen"; 
+$password = "patopatopato1"; 
+$database = "DiabetesUmg";
 
 $conn = new mysqli($host, $user, $password, $database);
 
